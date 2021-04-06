c2a1ed26d4c87c63e31e3dee86f63d1652c69300
Eduardo Dos Santos <eduardohenriquest@outlook.com>
2021-04-05 21:28:42 -0300

    add try catch

--- lib/services/world_time.dart
+++ lib/services/world_time.dart
@@ -15,19 +15,24 @@ class WorldTime {
 
   Future<void> getTime() async {
 
-    Response response = await get(Uri.parse('https://worldtimeapi.org/api/timezone/$url'));
+    try {
+      Response response = await get(Uri.parse('https://worldtimeapi.org/api/timezone/$url'));
 
-    Map data = jsonDecode(response.body);
+      Map data = jsonDecode(response.body);
 
-    String datetime = data['datetime'];
+      String datetime = data['datetime'];
 
-    String offset = data['utc_offset'].substring(1,3);
+      String offset = data['utc_offset'].substring(1,3);
 
-    DateTime now = DateTime.parse(datetime);
+      DateTime now = DateTime.parse(datetime);
 
-    now = now.add(Duration(hours: int.parse(offset)));
+      now = now.add(Duration(hours: int.parse(offset)));
 
-    time = now.toString();
+      time = now.toString();
+    } catch (e) {
+      print('Catch error: $e');
+      time = 'Could not get the time';
+    }
   }
 
 }
